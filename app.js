d9159ab2170588c22bd623e53b8aa1e78c4c7c3f
Mili Shandilya <orlovanskyinoy@gmail.com>
2025-06-01 15:42:48 +0530

    clinic registeration

--- app.js
+++ app.js
@@ -1,29 +1,31 @@
 const express = require("express");
 const { connectDB, sequelize } = require("./config/db");
+
 const User = require("./schema/userSchema");
 const Clinic = require('./schema/clinicSchema')
 const authRoutes = require('./routes/authRoute')
 const clinicRoute = require('./routes/clinicRoute')
 const cors = require('cors')
+const multer = require('multer')
+
+
+const upload = multer({
+  storage: multer.memoryStorage() 
+});
+
 const app = express();
 
 connectDB();
-app.use(express.urlencoded({ extended: true })); 
-app.use((req, res, next) => {
-  if (req.headers['content-type'] === 'application/json') {
-    express.json()(req, res, next);
-  } else {
-    next();
-  }
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
   origin: 'http://localhost:5173',
 }));
 
-
+app.use('/clinic', upload.single('image'), clinicRoute); 
 app.use('/clinic/auth',authRoutes)
-app.use('/clinic',clinicRoute)
+// app.use('/clinic',clinicRoute)
 
 
 sequelize.sync({ alter: true })
